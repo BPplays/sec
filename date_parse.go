6c2fdda1544f760f6265b6a57175b3fd69c23d0d
BPplays <andymalbp@gmail.com>
2024-03-06 04:43:21 -0800

    adding custom date parsing

--- date_parse.go
+++ date_parse.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"log"
+	"strconv"
 	"strings"
 	"time"
 )
+
+func exerr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 var date_sep = map[rune]bool{
 	'/': true,
@@ -24,29 +32,53 @@ var builders = map[string]strings.Builder{
 	"year": strings.Builder{},
 	"mon": strings.Builder{},
 	"day": strings.Builder{},
-	"time": strings.Builder{},
+	"hr": strings.Builder{},
+	"min": strings.Builder{},
+	"sec": strings.Builder{},
+}
+
+var builders_sep = map[string]map[rune]bool{
+	"year": date_sep,
+	"mon": date_sep,
+	"day": date_time_sep,
+	"hr": time_sep,
+	"min": time_sep,
+	"sec": time_sep,
 }
 
 
-func parse_date(in string) *time.Time {
+func parse_date(in string) time.Time {
+	var out_time time.Time
 
-	// var year strings.Builder
-	// var mon strings.Builder
-	// var day strings.Builder
-	// var time strings.Builder
-	// fmt.Println(input)
-	// runel := []rune(input)
-
-	for _, builder := range builders {
+	for builder_name, builder := range builders {
 		for _, i := range in {
-			if date_sep[i] {
+			if builders_sep[builder_name][i] {
 				continue
 			} else {
 				builder.WriteRune(i)
+				// builder.String()
 			}
 		}
 
 	}
 
+	year, err := strconv.Atoi(builders["year"].String())
+	exerr(err)
+	mon, err := strconv.Atoi(builders["mon"].String())
+	exerr(err)
+	day, err := strconv.Atoi(builders["day"].String())
+	exerr(err)
+	hr, err := strconv.Atoi(builders["hr"].String())
+	exerr(err)
+	min, err := strconv.Atoi(builders["min"].String())
+	exerr(err)
+	sec, err := strconv.Atoi(builders["sec"].String())
+	exerr(err)
+	// out_time.AddDate()
+	out_time = time.Date(year, time.Month(mon), day, hr, min, sec, 0, 0, time.UTC)
+	if err != nil {
+		log.Fatal(err)
+	}
 	// return strconv.ParseInt(sb.String(), 10, 64)
+	return out_time
 }
