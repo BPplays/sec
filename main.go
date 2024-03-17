1c2521250bf55aa097795c87a84df8c927795719
BPplays <andymalbp@gmail.com>
2024-03-16 23:06:46 -0700

    started change to big.int

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"math"
 	"math/big"
 	"sort"
+	"strconv"
 	"strings"
 	"time"
 	"unicode"
@@ -56,39 +57,39 @@ var AllPrefixes = map[string]Prefix{
 }
 
 var common_prefixes = map[string]Prefix{
-	"quetta": {Symbol: "Q", Base10: math.Pow(10, 30), FullName: "quetta", Adoption: 2022},
-	"ronna":  {Symbol: "R", Base10: math.Pow(10, 27), FullName: "ronna", Adoption: 2022},
-	"yotta":  {Symbol: "Y", Base10: math.Pow(10, 24), FullName: "yotta", Adoption: 1991},
-	"zetta":  {Symbol: "Z", Base10: math.Pow(10, 21), FullName: "zetta", Adoption: 1991},
-	"exa":    {Symbol: "E", Base10: math.Pow(10, 18), FullName: "exa", Adoption: 1975},
-	"peta":   {Symbol: "P", Base10: math.Pow(10, 15), FullName: "peta", Adoption: 1975},
-	"tera":   {Symbol: "T", Base10: math.Pow(10, 12), FullName: "tera", Adoption: 1960},
-	"giga":   {Symbol: "G", Base10: math.Pow(10, 9), FullName: "giga", Adoption: 1960},
-	"mega":   {Symbol: "M", Base10: math.Pow(10, 6), FullName: "mega", Adoption: 1873},
-	"kilo":   {Symbol: "k", Base10: math.Pow(10, 3), FullName: "kilo", Adoption: 1795},
-	"none":   {Symbol: "", Base10: math.Pow(10, 0), FullName: "none", Adoption: 1795},
-	"milli":  {Symbol: "m", Base10: math.Pow(10, -3), FullName: "milli", Adoption: 1795},
-	"micro":  {Symbol: "µ", Base10: math.Pow(10, -6), FullName: "micro", Adoption: 1873},
-	"nano":   {Symbol: "n", Base10: math.Pow(10, -9), FullName: "nano", Adoption: 1960},
-	"pico":   {Symbol: "p", Base10: math.Pow(10, -12), FullName: "pico", Adoption: 1960},
-	"femto":  {Symbol: "f", Base10: math.Pow(10, -15), FullName: "femto", Adoption: 1964},
-	"atto":   {Symbol: "a", Base10: math.Pow(10, -18), FullName: "atto", Adoption: 1964},
-	"zepto":  {Symbol: "z", Base10: math.Pow(10, -21), FullName: "zepto", Adoption: 1991},
-	"yocto":  {Symbol: "y", Base10: math.Pow(10, -24), FullName: "yocto", Adoption: 1991},
-	"ronto":  {Symbol: "r", Base10: math.Pow(10, -27), FullName: "ronto", Adoption: 2022},
-	"quecto": {Symbol: "q", Base10: math.Pow(10, -30), FullName: "quecto", Adoption: 2022},
+	"quetta": {Symbol: "Q", Base10: math.Pow(10, 30), Pow: 30, FullName: "quetta", Adoption: 2022},
+	"ronna":  {Symbol: "R", Base10: math.Pow(10, 27), Pow: 27, FullName: "ronna", Adoption: 2022},
+	"yotta":  {Symbol: "Y", Base10: math.Pow(10, 24), Pow: 24, FullName: "yotta", Adoption: 1991},
+	"zetta":  {Symbol: "Z", Base10: math.Pow(10, 21), Pow: 21, FullName: "zetta", Adoption: 1991},
+	"exa":    {Symbol: "E", Base10: math.Pow(10, 18), Pow: 18, FullName: "exa", Adoption: 1975},
+	"peta":   {Symbol: "P", Base10: math.Pow(10, 15), Pow: 15, FullName: "peta", Adoption: 1975},
+	"tera":   {Symbol: "T", Base10: math.Pow(10, 12), Pow: 12, FullName: "tera", Adoption: 1960},
+	"giga":   {Symbol: "G", Base10: math.Pow(10, 9), Pow: 9, FullName: "giga", Adoption: 1960},
+	"mega":   {Symbol: "M", Base10: math.Pow(10, 6), Pow: 6, FullName: "mega", Adoption: 1873},
+	"kilo":   {Symbol: "k", Base10: math.Pow(10, 3), Pow: 3, FullName: "kilo", Adoption: 1795},
+	"none":   {Symbol: "", Base10: math.Pow(10, 0), Pow: 0, FullName: "none", Adoption: 1795},
+	"milli":  {Symbol: "m", Base10: math.Pow(10, -3), Pow: -3, FullName: "milli", Adoption: 1795},
+	"micro":  {Symbol: "µ", Base10: math.Pow(10, -6), Pow: -6, FullName: "micro", Adoption: 1873},
+	"nano":   {Symbol: "n", Base10: math.Pow(10, -9), Pow: -9, FullName: "nano", Adoption: 1960},
+	"pico":   {Symbol: "p", Base10: math.Pow(10, -12), Pow: -12, FullName: "pico", Adoption: 1960},
+	"femto":  {Symbol: "f", Base10: math.Pow(10, -15), Pow: -15, FullName: "femto", Adoption: 1964},
+	"atto":   {Symbol: "a", Base10: math.Pow(10, -18), Pow: -18, FullName: "atto", Adoption: 1964},
+	"zepto":  {Symbol: "z", Base10: math.Pow(10, -21), Pow: -21, FullName: "zepto", Adoption: 1991},
+	"yocto":  {Symbol: "y", Base10: math.Pow(10, -24), Pow: -24, FullName: "yocto", Adoption: 1991},
+	"ronto":  {Symbol: "r", Base10: math.Pow(10, -27), Pow: -27, FullName: "ronto", Adoption: 2022},
+	"quecto": {Symbol: "q", Base10: math.Pow(10, -30), Pow: -30, FullName: "quecto", Adoption: 2022},
 }
 
 var leading_zero bool
 
-
+const max_pow int = 30
 const qsec_pow int64 = -30
 
-type times struct {
-	qsec big.Int // quecto sec
-	// sec int64
-	// asec int64 // atto sec 10 ^ -18. max prefix in signed int64
-}
+// type times struct {
+// 	qsec big.Int // quecto sec
+// 	// sec int64
+// 	// asec int64 // atto sec 10 ^ -18. max prefix in signed int64
+// }
 
 
 // func (t times) add(isec int64, iasec int64) {
@@ -120,7 +121,20 @@ func Digit(z *big.Int, digit int, pos int) {
 }
 
 
-func fmt_epoch_to_prefixsec(utime *big.Int, prefixesp *map[string]Prefix, break_prefix string, mul *float64) string {
+
+func padRunes(input []rune, length int) []rune {
+    if length <= len(input) {
+        return input
+    }
+    padding := make([]rune, length-len(input))
+    for i := range padding {
+        padding[i] = '0' // Assuming you want to pad with '0's
+    }
+    return append(padding, input...)
+}
+
+
+func fmt_epoch_to_prefixsec(utime *big.Int, prefixesp *map[string]Prefix, break_prefix string) string {
 	var output strings.Builder
 
 	// var fl_time float64
@@ -133,6 +147,8 @@ func fmt_epoch_to_prefixsec(utime *big.Int, prefixesp *map[string]Prefix, break_
 	// 	fl_time = float64(utime)
 	// }
 	str := []rune(utime.String())
+
+	fmt.Println(utime)
 
 
 	if round_power != 0 {
@@ -152,6 +168,7 @@ func fmt_epoch_to_prefixsec(utime *big.Int, prefixesp *map[string]Prefix, break_
 	// var fl_round_time float64
 
 
+	str = padRunes(str, max_pow+12) // why is +12 needed?
 
 
 	keys := make([]string, 0, len(prefixes))
@@ -168,26 +185,65 @@ func fmt_epoch_to_prefixsec(utime *big.Int, prefixesp *map[string]Prefix, break_
 
 	var value Prefix
 	var next_value Prefix
-	var powerDifference float64
+	var powerDifference int64
 
-	// var first_non0 bool = false
+	var first_non0 bool = false
 	// Iterate over the sorted keys
 
 	// var last_power int64
+	// stln := int64(len(str))
+	var tmp string
+	var tmpn int
+	var err error
 	for i, key := range keys {
 		value = prefixes[key]
 
 		if i+1 < len(prefixes) {
 			next_value = prefixes[keys[i+1]]
 
-			powerDifference = math.Log10(value.Base10) - math.Log10(next_value.Base10)
+			// powerDifference = math.Log10(value.Base10) - math.Log10(next_value.Base10)
+			powerDifference = value.Pow - next_value.Pow
 		} else {
 			powerDifference = 3
 		}
 
-		output.WriteString(string(str[:int(math.Round(powerDifference))]))
-		output.WriteString(value.Symbol+"s")
-		str = str[:int(math.Round(powerDifference))]
+		if len(str) < int(powerDifference) {
+			break
+		}
+
+
+		// fmt.Println(value.Pow, len(str), powerDifference, string(str))
+		// fmt.Println((value.Pow + qsec_pow), stln)
+
+		// if (value.Pow) <= stln {
+		// 	continue
+		// }
+		tmp = string(str[:int(powerDifference)])
+
+		tmpn, err = strconv.Atoi(tmp)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if tmpn != 0 {
+			// fmt.Println(tmpn)
+
+			if leading_zero && ((!(leading_zero_start_from_sec && !first_non0))) {
+				// formatString := fmt.Sprintf("%%0%d.0f%%v", int(powerDifference))
+				// fmt.Println(formatString)
+				output.WriteString(fmt.Sprintf("%0*d%v", powerDifference ,tmpn, value.Symbol+"s "))
+			} else {
+				output.WriteString(fmt.Sprintf("%v%v",tmpn, value.Symbol+"s "))
+			}
+			// output.WriteString(tmp)
+			// output.WriteString(value.Symbol+"s ")
+			
+			first_non0 = true
+		}
+		str = str[int(powerDifference):]
+
+
+
 
 
 
@@ -423,15 +479,15 @@ func main() {
 	baresec := viper.GetBool("bare")
 
 	// Get the current time in UTC
-	currentTime := time.Now().UTC()
+	// currentTime := time.Now().UTC()
 
 	// Get the Unix epoch time in seconds
 
-	var mul_val float64 = 1
+	// var mul_val float64 = 1
 	
-	var mul *float64
+	// var mul *float64
 
-	mul = &mul_val
+	// mul = &mul_val
 
 	if viper.IsSet("prefix_second") {
 		utimeValue := viper.GetString("prefix_second")
@@ -442,21 +498,21 @@ func main() {
 	last_prefix = "none"
 
 	if millisec {
-		epochTime = currentTime.UnixMilli()
+		// epochTime = currentTime.UnixMilli()
 		last_prefix = "milli"
-		*(mul) = math.Pow(10, -3)
+		// *(mul) = math.Pow(10, -3)
 		// time.Unix()
 	} else if microsec {
-		epochTime = currentTime.UnixMicro()
+		// epochTime = currentTime.UnixMicro()
 		last_prefix = "micro"
-		*(mul) = math.Pow(10, -6)
+		// *(mul) = math.Pow(10, -6)
 	} else if nanosec {
-		epochTime = currentTime.UnixNano()
+		// epochTime = currentTime.UnixNano()
 		last_prefix = "nano"
-		*(mul) = math.Pow(10, -9)
+		// *(mul) = math.Pow(10, -9)
 	} else {
-		epochTime = currentTime.Unix()
-		mul = nil
+		// epochTime = currentTime.Unix()
+		// mul = nil
 	}
 
 
@@ -481,25 +537,26 @@ func main() {
 		
 		// fmt.Println(parsed_date)
 		// fmt.Println(parsed_date.Unix())
-		// if millisec {
-		// 	epochTime = parsed_date.UnixMilli()
-		// 	last_prefix = "milli"
-		// 	*(mul) = math.Pow(10, -3)
-		// } else if microsec {
-		// 	epochTime = parsed_date.UnixMicro()
-		// 	last_prefix = "micro"
-		// 	*(mul) = math.Pow(10, -6)
-		// } else if nanosec {
-		// 	epochTime = parsed_date.UnixNano()
-		// 	last_prefix = "nano"
-		// 	*(mul) = math.Pow(10, -9)
-		// } else {
-		// 	epochTime = parsed_date.Unix()
-		// 	parsed_date.UTC().Nanosecond()
-		// 	mul = nil
-		// }
+		if millisec {
+			// epochTime = parsed_date.UnixMilli()
+			last_prefix = "milli"
+			// *(mul) = math.Pow(10, -3)
+		} else if microsec {
+			// epochTime = parsed_date.UnixMicro()
+			last_prefix = "micro"
+			// *(mul) = math.Pow(10, -6)
+		} else if nanosec {
+			// epochTime = parsed_date.UnixNano()
+			last_prefix = "nano"
+			// *(mul) = math.Pow(10, -9)
+		} else {
+			// epochTime = parsed_date.Unix()
+			// parsed_date.UTC().Nanosecond()
+			// mul = nil
+		}
 		epochTime = parsed_date.Unix()
 		ns := parsed_date.UTC().Nanosecond()
+		fmt.Println(ns)
 		utime = big.NewInt(epochTime)
 		tmp := big.NewInt(0)
 		tmp2 := big.NewInt(0)
@@ -536,7 +593,7 @@ func main() {
 			if last_prefix_override != "none" {
 				last_prefix = last_prefix_override
 			}
-			fmt.Println(fmt_epoch_to_prefixsec(utime, prefix_to_use, last_prefix, mul))
+			fmt.Println(fmt_epoch_to_prefixsec(utime, prefix_to_use, last_prefix))
 		}
 		
 	}
@@ -544,7 +601,7 @@ func main() {
 	if benchmark {
 		for i := 0; i < 100000; i++ {
 			// Your loop body code goes here
-			fmt_epoch_to_prefixsec(utime, prefix_to_use, last_prefix, mul)
+			fmt_epoch_to_prefixsec(utime, prefix_to_use, last_prefix)
 		}
 	}
 
